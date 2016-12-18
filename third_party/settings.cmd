4cc24a33f6aba39edfa5565f4ecb021f9cb4ac2c
Alexej Harm <alexej.h@xiphos.de>
2016-12-18 13:44:17 +0100

    fixed third_party dependencies

---
+++ third_party/settings.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+rem Generator
+rem set generator=Visual Studio 15 2017
+set generator=Visual Studio 15 2017 Win64
+
+rem Toolset
+rem set toolset=v141_clang_c2
+set toolset=v141
+
+rem Configs
+set configs=Release;Debug
